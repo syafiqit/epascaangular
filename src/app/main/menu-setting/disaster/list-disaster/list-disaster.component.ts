451b830ace22556591466035d13dd60034bdee30
Firdaus Manaf <muhdfirdausam@gmail.com>
2021-02-04 11:09:24 +0800

    feat: design ui for list disaster

--- src/app/main/menu-setting/disaster/list-disaster/list-disaster.component.ts
+++ src/app/main/menu-setting/disaster/list-disaster/list-disaster.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ColumnMode, SortType } from '@swimlane/ngx-datatable';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list-disaster',
-  templateUrl: './list-disaster.component.html'
+  templateUrl: './list-disaster.component.html',
+  encapsulation: ViewEncapsulation.None,
+  providers: [NgbModalConfig, NgbModal]
 })
 export class ListDisasterComponent implements OnInit {
 
-  constructor() { }
+  rows = [
+    {"date" : "12/12/2020",  "disaster": "Banjir", "notes": "Gelombang 1"},
+    {"date" : "20/12/2020",  "disaster": "Covid-19", "notes": "Kluster 1 & 2"},
+    {"date" : "3/1/2021", "disaster": "Covid-19", "notes": "Kluster 3"}
+
+  ]
+
+  ColumnMode = ColumnMode;
+  SortType = SortType;
+
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit(): void {
   }
