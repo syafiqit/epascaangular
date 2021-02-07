777e36a661aec7e1d5b73b62825296707f32dcac
syafiqit <syafiq.asyraf.it@gmail.com>
2021-02-08 07:57:27 +0800

    fix: add placeholder for form, rearrang form and fix ui

--- src/app/main/admin/victim-profile/add-victim/add-victim.component.ts
+++ src/app/main/admin/victim-profile/add-victim/add-victim.component.ts
@@ -8,6 +8,26 @@ import { of } from 'rxjs/internal/observable/of';
 })
 export class AddVictimComponent implements OnInit {
 
+  dun = [
+    { "data": "DUN Bukau",},
+    { "data": "DUN Mentari",},
+  ];
+
+  districts = [
+    { "data": "Tebrau",},
+    { "data": "Kuala Pilah",},
+  ];
+
+  parliaments = [
+    { "data": "Kuantan",},
+    { "data": "Seremban",},
+  ];
+
+  states = [
+    { "data": "Pahang",},
+    { "data": "Selangor",},
+  ];
+
   options$: Observable<number[]>;
   constructor() {
     this.options$=of([1,2,3,4,5,6]);
--- src/app/main/admin/victim-profile/edit-victim/victim-profile/victim-profile.component.ts
+++ src/app/main/admin/victim-profile/edit-victim/victim-profile/victim-profile.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VictimProfileComponent implements OnInit {
 
+  dun = [
+    { "data": "DUN Bukau",},
+    { "data": "DUN Mentari",},
+  ];
+
+  districts = [
+    { "data": "Tebrau",},
+    { "data": "Kuala Pilah",},
+  ];
+
+  parliaments = [
+    { "data": "Kuantan",},
+    { "data": "Seremban",},
+  ];
+
+  states = [
+    { "data": "Pahang",},
+    { "data": "Selangor",},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
