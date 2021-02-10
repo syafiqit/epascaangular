06ab2053178f4ec2c5e5490db383890605f26abe
Zaki Zakaria <zakihelmi1997@gmail.com>
2021-02-10 11:48:15 +0800

    fix: modify route for skb and direct-payment

---
+++ src/app/main/fund-management/payment/skb/edit-skb/edit-skb.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ColumnMode, SortType } from '@swimlane/ngx-datatable';
+
+@Component({
+  selector: 'app-edit-skb',
+  templateUrl: './edit-skb.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class EditSkbComponent implements OnInit {
+
+  rows = [
+    {"month":"Januari", "total":""},{"month":"Februari", "total":""},{"month":"Mac", "total":""},
+    {"month":"April", "total":""},{"month":"Mei", "total":""},{"month":"Jun", "total":""},
+    {"month":"Julai", "total":""},{"month":"Ogos", "total":""},{"month":"September", "total":""},
+    {"month":"Oktober", "total":""},{"month":"November", "total":""},{"month":"Disember", "total":""},
+    {"month":"Jumlah Belanja", "total":""}
+]
+
+ColumnMode = ColumnMode;
+SortType = SortType;
+
+displayMonths = 1;
+navigation = 'select';
+showWeekNumbers = false;
+outsideDays = 'visible';
+
+constructor() { }
+
+ngOnInit(): void {
+}
+
+}
