00842cd47a3f0dbd4cdbf915a3ec2e359c3a147b
Firdaus Manaf <muhdfirdausam@gmail.com>
2021-02-15 14:37:22 +0800

    feat: modify table month in add skb page

--- src/app/main/fund-management/payment/skb/add-skb/add-skb.component.ts
+++ src/app/main/fund-management/payment/skb/add-skb/add-skb.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { ColumnMode, SortType } from '@swimlane/ngx-datatable';
-
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
+import { LazyLoadEvent } from 'primeng/api';
+import { Paginator } from 'primeng/paginator';
+import { Table } from 'primeng/table';
+import { PrimengTableHelper } from 'src/app/shared/helpers/PrimengTableHelper';
 @Component({
   selector: 'app-add-skb',
   templateUrl: './add-skb.component.html',
   encapsulation: ViewEncapsulation.None
 })
 export class AddSkbComponent implements OnInit {
+
+  @ViewChild('dataTable', { static: true }) dataTable: Table;
+  @ViewChild('paginator', { static: true }) paginator: Paginator;
+
+  primengTableHelper: PrimengTableHelper;
 
   rows = [
     {"month":"Januari", "total":""},{"month":"Februari", "total":""},{"month":"Mac", "total":""},
@@ -16,17 +23,32 @@ export class AddSkbComponent implements OnInit {
     {"month":"Jumlah Belanja", "total":""}
 ]
 
-ColumnMode = ColumnMode;
-SortType = SortType;
-
 displayMonths = 1;
 navigation = 'select';
 showWeekNumbers = false;
 outsideDays = 'visible';
 
-constructor() { }
+constructor() {
+  this.primengTableHelper = new PrimengTableHelper();
+}
 
 ngOnInit(): void {
+}
+
+getAddSKB(event?: LazyLoadEvent) {
+  if (this.primengTableHelper.shouldResetPaging(event)) {
+    this.paginator.changePage(0);
+    return;
+  }
+
+  this.primengTableHelper.showLoadingIndicator();
+  this.primengTableHelper.totalRecordsCount = this.rows.length;
+  this.primengTableHelper.records = this.rows;
+  this.primengTableHelper.hideLoadingIndicator();
+}
+
+reloadPage(): void {
+  this.paginator.changePage(this.paginator.getPage());
 }
 
 }
