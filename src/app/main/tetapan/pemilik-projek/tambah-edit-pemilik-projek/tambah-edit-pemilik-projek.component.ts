23a524a0270cd7f59b283a4d7073a55b4de1c639
Acipzzz <hasif.yaacob98@gmail.com>
2021-03-31 23:58:03 +0800

    feat: consume api tetapan pemilik projek

--- src/app/main/tetapan/pemilik-projek/tambah-edit-pemilik-projek/tambah-edit-pemilik-projek.component.ts
+++ src/app/main/tetapan/pemilik-projek/tambah-edit-pemilik-projek/tambah-edit-pemilik-projek.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { NgbActiveModal, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { finalize } from 'rxjs/operators';
+import { CreateOrEditRefPemilikDto, RefPemilikServiceProxy } from 'src/app/shared/proxy/service-proxies';
+declare let require;
+const Swal = require('sweetalert2');
 
 @Component({
 	selector: 'app-tambah-edit-pemilik-projek',
@@ -7,8 +11,51 @@ import { NgbActiveModal, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-boots
 	encapsulation: ViewEncapsulation.None,
 	providers: [NgbModalConfig, NgbModal]
 })
-export class TambahEditPemilikProjekComponent implements OnInit {
-	constructor(private modalService: NgbModal, public activeModal: NgbActiveModal) {}
 
-	ngOnInit(): void {}
+export class TambahEditPemilikProjekComponent implements OnInit {
+	@Input() name;
+	@Input() id;
+
+	pemilik: CreateOrEditRefPemilikDto = new CreateOrEditRefPemilikDto();
+	saving = true;
+
+	constructor(
+		private modalService: NgbModal, 
+		public activeModal: NgbActiveModal,
+		private _refPemilikServiceProxy: RefPemilikServiceProxy
+	) {}
+
+	ngOnInit(): void {
+		this.show();
+	}
+	
+	show() {
+		if (!this.id) {
+			this.pemilik = new CreateOrEditRefPemilikDto();
+		} else {
+			this._refPemilikServiceProxy.getRefPemilikForEdit(this.id).subscribe((result) => {
+				this.pemilik = result.ref_pemilik;
+			});
+		}
+	}
+	
+	save(): void {
+		this.saving = true;
+
+		this._refPemilikServiceProxy
+			.createOrEdit(this.pemilik)
+			.pipe(
+				finalize(() => {
+					this.saving = false;
+				})
+			)
+			.subscribe(() => {
+				if (this.name == 'add') {
+					Swal.fire('Berjaya!', 'Maklumat Pemilik Projek Berjaya Di Tambah.', 'success');
+				} else if (this.name == 'edit') {
+					Swal.fire('Berjaya!', 'Maklumat Pemilik Projek Berjaya Di Ubah.', 'success');
+				}
+				this.activeModal.close(true);
+			});
+	}
 }
