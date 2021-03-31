90d6768c19e4dd45a0f8c17708573b93dc3b6c69
Acipzzz <hasif.yaacob98@gmail.com>
2021-03-31 23:56:07 +0800

    feat: consume api tetapan jenis bantuan

--- src/app/main/tetapan/jenis-bantuan/tambah-edit-jenis-bantuan/tambah-edit-jenis-bantuan.component.ts
+++ src/app/main/tetapan/jenis-bantuan/tambah-edit-jenis-bantuan/tambah-edit-jenis-bantuan.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { NgbActiveModal, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
-import { ToastrService } from 'ngx-toastr';
+import { finalize } from 'rxjs/operators';
+import { CreateOrEditRefBantuanDto, RefBantuanServiceProxy } from 'src/app/shared/proxy/service-proxies';
+declare let require;
+const Swal = require('sweetalert2');
 
 @Component({
 	selector: 'app-tambah-edit-jenis-bantuan',
@@ -8,12 +11,51 @@ import { ToastrService } from 'ngx-toastr';
 	encapsulation: ViewEncapsulation.None,
 	providers: [NgbModalConfig, NgbModal]
 })
+
 export class TambahEditJenisBantuanComponent implements OnInit {
 	@Input() name;
+	@Input() id;
 
-	constructor(private modalService: NgbModal, public activeModal: NgbActiveModal, private toaster: ToastrService) {}
+	bantuan: CreateOrEditRefBantuanDto = new CreateOrEditRefBantuanDto();
+	saving = true;
 
-	ngOnInit(): void {}
+	constructor(
+		private modalService: NgbModal, 
+		public activeModal: NgbActiveModal, 
+		private _refBantuanServiceProxy: RefBantuanServiceProxy
+	) {}
 
-	status = [{ status: 'Aktif' }, { status: 'Tidak Aktif' }];
+	ngOnInit(): void {
+		this.show();
+	}
+	
+	show() {
+		if (!this.id) {
+			this.bantuan = new CreateOrEditRefBantuanDto();
+		} else {
+			this._refBantuanServiceProxy.getRefBantuanForEdit(this.id).subscribe((result) => {
+				this.bantuan = result.ref_bantuan;
+			});
+		}
+	}
+	
+	save(): void {
+		this.saving = true;
+
+		this._refBantuanServiceProxy
+			.createOrEdit(this.bantuan)
+			.pipe(
+				finalize(() => {
+					this.saving = false;
+				})
+			)
+			.subscribe(() => {
+				if (this.name == 'add') {
+					Swal.fire('Berjaya!', 'Maklumat Jenis Bantuan Berjaya Di Tambah.', 'success');
+				} else if (this.name == 'edit') {
+					Swal.fire('Berjaya!', 'Maklumat Jenis Bantuan Berjaya Di Ubah.', 'success');
+				}
+				this.activeModal.close(true);
+			});
+	}
 }
