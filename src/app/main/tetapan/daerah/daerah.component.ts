438acebc1f2f0f16cd618e46f7e914f70d802f53
syafiqit <syafiq.asyraf.it@gmail.com>
2021-03-31 15:34:01 +0800

    feat: consume api for tetapan daerah

--- src/app/main/tetapan/daerah/daerah.component.ts
+++ src/app/main/tetapan/daerah/daerah.component.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import { LazyLoadEvent } from 'primeng/api';
 import { Paginator } from 'primeng/paginator';
 import { Table } from 'primeng/table';
 import { PrimengTableHelper } from 'src/app/shared/helpers/PrimengTableHelper';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { TambahEditDaerahComponent } from './tambah-edit-daerah/tambah-edit-daerah.component';
+import {RefDaerahServiceProxy, RefNegeriServiceProxy} from "../../../shared/proxy/service-proxies";
 
 @Component({
 	selector: 'app-daerah',
@@ -12,47 +13,91 @@ import { TambahEditDaerahComponent } from './tambah-edit-daerah/tambah-edit-daer
 	encapsulation: ViewEncapsulation.None,
 	providers: [NgbModalConfig, NgbModal]
 })
-export class DaerahComponent implements AfterViewInit {
+export class DaerahComponent implements OnInit {
 	@ViewChild('dataTable', { static: true }) dataTable: Table;
 	@ViewChild('paginator', { static: true }) paginator: Paginator;
 
 	primengTableHelper: PrimengTableHelper;
 
-	rows = [
-		{ district: 'Bentong', state: 'Pahang', status: 'Aktif' },
-		{ district: 'Bera', state: 'Pahang', status: 'Aktif' },
-		{ district: 'Jerantut', state: 'Pahang', status: 'Aktif' },
-		{ district: 'Lipis', state: 'Pahang', status: 'Aktif' },
-		{ district: 'Raub', state: 'Pahang', status: 'Aktif' }
-	];
+  filterText = '';
+  filter: any;
+  sorting: any;
+  skipCount: any;
+  maxResultCount: any;
+  states: any[];
 
-	constructor(config: NgbModalConfig, private modalService: NgbModal) {
+	constructor(
+	  config: NgbModalConfig,
+    private modalService: NgbModal,
+    private _refDaerahServiceProxy: RefDaerahServiceProxy,
+    private _refNegeriServiceProxy: RefNegeriServiceProxy
+  ) {
 		this.primengTableHelper = new PrimengTableHelper();
 		config.backdrop = 'static';
 		config.keyboard = false;
 	}
 
-	ngAfterViewInit(): void {
-		//this.primengTableHelper.adjustScroll(this.dataTable);
+	ngOnInit(): void {
+		this.negeri()
 	}
 
-	getApplication(event?: LazyLoadEvent) {
-		if (this.primengTableHelper.shouldResetPaging(event)) {
-			this.paginator.changePage(0);
-			return;
-		}
+	getDaerah(event?: LazyLoadEvent) {
+    if (this.primengTableHelper.shouldResetPaging(event)) {
+      this.paginator.changePage(0);
+      return;
+    }
 
-		this.primengTableHelper.showLoadingIndicator();
-		this.primengTableHelper.totalRecordsCount = this.rows.length;
-		this.primengTableHelper.records = this.rows;
-		this.primengTableHelper.hideLoadingIndicator();
+    this.primengTableHelper.showLoadingIndicator();
+    this._refDaerahServiceProxy
+      .getAll(
+        this.filterText,
+        this.primengTableHelper.getSorting(this.dataTable),
+        this.primengTableHelper.getSkipCount(this.paginator, event),
+        this.primengTableHelper.getMaxResultCount(this.paginator, event)
+      )
+      .subscribe((result) => {
+        this.primengTableHelper.totalRecordsCount = result.total_count;
+        this.primengTableHelper.records = result.items;
+        this.primengTableHelper.hideLoadingIndicator();
+      });
 	}
 
 	reloadPage(): void {
 		this.paginator.changePage(this.paginator.getPage());
 	}
 
+  negeri() {
+    this._refNegeriServiceProxy
+      .getRefNegeriForDropdown(this.filter)
+      .subscribe((result) => {
+        this.states = result.items.map((data) => {
+          return data.nama_negeri;
+        });
+      });
+  }
+
+  getNegeri(id) {
+    return this.states[id - 1];
+  }
+
 	addDistrictModal() {
-		this.modalService.open(TambahEditDaerahComponent, { size: 'lg' });
+    const modalRef = this.modalService.open(TambahEditDaerahComponent, { size: 'lg' });
+    modalRef.componentInstance.name = 'add';
+    modalRef.result.then((response) => {
+      if (response) {
+        this.getDaerah();
+      }
+    });
 	}
+
+  editDistrictModal(id) {
+    const modalRef = this.modalService.open(TambahEditDaerahComponent, { size: 'lg' });
+    modalRef.componentInstance.name = 'edit';
+    modalRef.componentInstance.id = id;
+    modalRef.result.then((response) => {
+      if (response) {
+        this.getDaerah();
+      }
+    });
+  }
 }
