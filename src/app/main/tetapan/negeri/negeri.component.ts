57c51b3509e483704ff24db27391aaa5d2bb1f80
syafiqit <syafiq.asyraf.it@gmail.com>
2021-04-01 09:20:17 +0800

    feat: consume api for tetapan negeri

--- src/app/main/tetapan/negeri/negeri.component.ts
+++ src/app/main/tetapan/negeri/negeri.component.ts
@@ -1,54 +1,80 @@
-import { AfterViewInit, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import { LazyLoadEvent } from 'primeng/api';
 import { Paginator } from 'primeng/paginator';
 import { Table } from 'primeng/table';
 import { PrimengTableHelper } from 'src/app/shared/helpers/PrimengTableHelper';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { TambahEditNegeriComponent } from './tambah-edit-negeri/tambah-edit-negeri.component';
+import {RefNegeriServiceProxy} from "../../../shared/proxy/service-proxies";
 
 @Component({
 	selector: 'app-negeri',
 	templateUrl: './negeri.component.html',
 	encapsulation: ViewEncapsulation.None
 })
-export class NegeriComponent implements AfterViewInit {
+export class NegeriComponent implements OnInit {
 	@ViewChild('dataTable', { static: true }) dataTable: Table;
 	@ViewChild('paginator', { static: true }) paginator: Paginator;
 
 	primengTableHelper: PrimengTableHelper;
 
-	rows = [
-		{ state: 'Terengganu', status: 'Aktif' },
-		{ state: 'Kuala Lumpur', status: 'Aktif' }
-	];
+  filterText = '';
 
-	constructor(config: NgbModalConfig, private modalService: NgbModal) {
-		this.primengTableHelper = new PrimengTableHelper();
-		config.backdrop = 'static';
-		config.keyboard = false;
-	}
+  constructor(
+    config: NgbModalConfig,
+    private modalService: NgbModal,
+    private _refNegeriServiceProxy: RefNegeriServiceProxy
+  ) {
+    this.primengTableHelper = new PrimengTableHelper();
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
-	ngAfterViewInit(): void {
-		//this.primengTableHelper.adjustScroll(this.dataTable);
-	}
+  ngOnInit(): void {}
 
-	getApplication(event?: LazyLoadEvent) {
-		if (this.primengTableHelper.shouldResetPaging(event)) {
-			this.paginator.changePage(0);
-			return;
-		}
+	getNegeri(event?: LazyLoadEvent) {
+    if (this.primengTableHelper.shouldResetPaging(event)) {
+      this.paginator.changePage(0);
+      return;
+    }
 
-		this.primengTableHelper.showLoadingIndicator();
-		this.primengTableHelper.totalRecordsCount = this.rows.length;
-		this.primengTableHelper.records = this.rows;
-		this.primengTableHelper.hideLoadingIndicator();
+    this.primengTableHelper.showLoadingIndicator();
+    this._refNegeriServiceProxy
+      .getAll(
+        this.filterText,
+        this.primengTableHelper.getSorting(this.dataTable),
+        this.primengTableHelper.getSkipCount(this.paginator, event),
+        this.primengTableHelper.getMaxResultCount(this.paginator, event)
+      )
+      .subscribe((result) => {
+        this.primengTableHelper.totalRecordsCount = result.total_count;
+        this.primengTableHelper.records = result.items;
+        this.primengTableHelper.hideLoadingIndicator();
+      });
 	}
 
 	reloadPage(): void {
 		this.paginator.changePage(this.paginator.getPage());
 	}
 
-	addStateModal() {
-		this.modalService.open(TambahEditNegeriComponent, { size: 'lg' });
-	}
+  addStateModal() {
+    const modalRef = this.modalService.open(TambahEditNegeriComponent, { size: 'lg' });
+    modalRef.componentInstance.name = 'add';
+    modalRef.result.then((response) => {
+      if (response) {
+        this.getNegeri();
+      }
+    });
+  }
+
+  editStateModal(id) {
+    const modalRef = this.modalService.open(TambahEditNegeriComponent, { size: 'lg' });
+    modalRef.componentInstance.name = 'edit';
+    modalRef.componentInstance.id = id;
+    modalRef.result.then((response) => {
+      if (response) {
+        this.getNegeri();
+      }
+    });
+  }
 }
--- src/app/main/tetapan/negeri/tambah-edit-negeri/tambah-edit-negeri.component.ts
+++ src/app/main/tetapan/negeri/tambah-edit-negeri/tambah-edit-negeri.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import { NgbActiveModal, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import {CreateOrEditRefNegeriDto, RefNegeriServiceProxy} from "../../../../shared/proxy/service-proxies";
+declare let require;
+const Swal = require('sweetalert2');
 
 @Component({
 	selector: 'app-tambah-edit-negeri',
@@ -8,7 +11,58 @@ import { NgbActiveModal, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-boots
 	providers: [NgbModalConfig, NgbModal]
 })
 export class TambahEditNegeriComponent implements OnInit {
-	constructor(private modalService: NgbModal, public activeModal: NgbActiveModal) {}
+  @Input() name;
+  @Input() id;
 
-	ngOnInit(): void {}
+  negeri: CreateOrEditRefNegeriDto = new CreateOrEditRefNegeriDto();
+  saving = false;
+
+  filterNegeri: any;
+  sortingNegeri: any;
+  skipCountNegeri: any;
+  maxResultCountNegeri: any;
+  states: any[];
+
+  constructor(
+    private modalService: NgbModal,
+    public activeModal: NgbActiveModal,
+    private _refNegeriServiceProxy: RefNegeriServiceProxy
+  ) {}
+
+  ngOnInit(): void {
+    this.getNegeri();
+    this.show();
+  }
+
+  getNegeri(filter?) {
+    this._refNegeriServiceProxy.getRefNegeriForDropdown(filter).subscribe((result) => {
+      this.states = result.items;
+    });
+  }
+
+  show() {
+    if (!this.id) {
+      this.negeri = new CreateOrEditRefNegeriDto();
+    } else {
+      this._refNegeriServiceProxy.getRefNegeriForEdit(this.id).subscribe((result) => {
+        this.negeri = result.ref_negeri;
+      });
+    }
+  }
+
+  save(): void {
+    this.saving = true;
+
+    this._refNegeriServiceProxy
+      .createOrEdit(this.negeri)
+      .pipe()
+      .subscribe(() => {
+        if (this.name == 'add') {
+          Swal.fire('Berjaya!', 'Maklumat Negeri Berjaya Di Tambah.', 'success');
+        } else if (this.name == 'edit') {
+          Swal.fire('Berjaya!', 'Maklumat Negeri Berjaya Di Ubah.', 'success');
+        }
+        this.activeModal.close(true);
+      });
+  }
 }
