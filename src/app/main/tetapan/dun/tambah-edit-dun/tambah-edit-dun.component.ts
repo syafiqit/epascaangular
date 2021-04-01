123ceff8af9bf5cabd204809342562f0e68da3ea
syafiqit <syafiq.asyraf.it@gmail.com>
2021-04-01 09:17:14 +0800

    feat: consume api for tetapan dun

--- src/app/main/tetapan/dun/tambah-edit-dun/tambah-edit-dun.component.ts
+++ src/app/main/tetapan/dun/tambah-edit-dun/tambah-edit-dun.component.ts
@@ -1,14 +1,76 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  CreateOrEditRefDunDto,
+  RefDunServiceProxy,
+  RefNegeriServiceProxy,
+  RefParlimenServiceProxy
+} from "../../../../shared/proxy/service-proxies";
+declare let require;
+const Swal = require('sweetalert2');
 
 @Component({
 	selector: 'app-tambah-edit-dun',
 	templateUrl: './tambah-edit-dun.component.html'
 })
 export class TambahEditDunComponent implements OnInit {
-	@Input() name;
+  @Input() name;
+  @Input() id;
 
-	constructor(private modalService: NgbModal, public activeModal: NgbActiveModal) {}
+  dun: CreateOrEditRefDunDto = new CreateOrEditRefDunDto();
+  saving = false;
+  states: any[];
+  parliament: any[];
 
-	ngOnInit(): void {}
+  constructor(
+    private modalService: NgbModal,
+    public activeModal: NgbActiveModal,
+    private _refDunServiceProxy: RefDunServiceProxy,
+    private _refNegeriServiceProxy: RefNegeriServiceProxy,
+    private _refParlimenServiceProxy: RefParlimenServiceProxy
+  ) {}
+
+  ngOnInit(): void {
+    this.show();
+    this.getNegeri();
+    this.getParlimen();
+  }
+
+  getParlimen(filter?) {
+    this._refParlimenServiceProxy.getRefParlimenForDropdown(filter).subscribe((result) => {
+      this.parliament = result.items;
+    });
+  }
+
+  getNegeri(filter?) {
+    this._refNegeriServiceProxy.getRefNegeriForDropdown(filter).subscribe((result) => {
+      this.states = result.items;
+    });
+  }
+
+  show() {
+    if (!this.id) {
+      this.dun = new CreateOrEditRefDunDto();
+    } else {
+      this._refDunServiceProxy.getRefDunForEdit(this.id).subscribe((result) => {
+        this.dun = result.ref_dun;
+      });
+    }
+  }
+
+  save(): void {
+    this.saving = true;
+
+    this._refDunServiceProxy
+      .createOrEdit(this.dun)
+      .pipe()
+      .subscribe(() => {
+        if (this.name == 'add') {
+          Swal.fire('Berjaya!', 'Maklumat Dun Berjaya Di Tambah.', 'success');
+        } else if (this.name == 'edit') {
+          Swal.fire('Berjaya!', 'Maklumat Dun Berjaya Di Ubah.', 'success');
+        }
+        this.activeModal.close(true);
+      });
+  }
 }
