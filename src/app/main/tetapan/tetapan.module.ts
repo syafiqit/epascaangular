0f012f0be659dd7817b1ef4aa5c06feb34c3bc14
syafiqit <syafiq.asyraf.it@gmail.com>
2021-04-01 17:03:38 +0800

    chores: perform linting

--- src/app/main/tetapan/tetapan.module.ts
+++ src/app/main/tetapan/tetapan.module.ts
@@ -43,10 +43,18 @@ import { PerananComponent } from './peranan/peranan.component';
 import { TambahEditPerananComponent } from './peranan/tambah-edit-peranan/tambah-edit-peranan.component';
 import { RujukanComponent } from './rujukan/rujukan.component';
 import { TambahEditRujukanComponent } from './rujukan/tambah-edit-rujukan/tambah-edit-rujukan.component';
-import { NgSelectModule } from "@ng-select/ng-select";
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, TetapanRoutingModule, NgxDatatableModule, TableModule, PaginatorModule, NgSelectModule],
+	imports: [
+		CommonModule,
+		SharedModule,
+		TetapanRoutingModule,
+		NgxDatatableModule,
+		TableModule,
+		PaginatorModule,
+		NgSelectModule
+	],
 	declarations: [
 		PengumumanComponent,
 		TambahEditPengumumanComponent,
