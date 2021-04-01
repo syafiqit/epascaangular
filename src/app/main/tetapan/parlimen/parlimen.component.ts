638f9e2a562364e390d3c88560c6f1867dd3d18d
syafiqit <syafiq.asyraf.it@gmail.com>
2021-04-01 09:21:53 +0800

    feat: consume api for tetapan parlimen

--- src/app/main/tetapan/parlimen/parlimen.component.ts
+++ src/app/main/tetapan/parlimen/parlimen.component.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import { LazyLoadEvent } from 'primeng/api';
 import { Paginator } from 'primeng/paginator';
 import { Table } from 'primeng/table';
 import { PrimengTableHelper } from 'src/app/shared/helpers/PrimengTableHelper';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { TambahEditParlimenComponent } from './tambah-edit-parlimen/tambah-edit-parlimen.component';
+import {RefNegeriServiceProxy, RefParlimenServiceProxy} from "../../../shared/proxy/service-proxies";
 
 @Component({
 	selector: 'app-parlimen',
@@ -12,45 +13,89 @@ import { TambahEditParlimenComponent } from './tambah-edit-parlimen/tambah-edit-
 	encapsulation: ViewEncapsulation.None,
 	providers: [NgbModalConfig, NgbModal]
 })
-export class ParlimenComponent implements AfterViewInit {
-	@ViewChild('dataTable', { static: true }) dataTable: Table;
-	@ViewChild('paginator', { static: true }) paginator: Paginator;
+export class ParlimenComponent implements OnInit {
+  @ViewChild('dataTable', { static: true }) dataTable: Table;
+  @ViewChild('paginator', { static: true }) paginator: Paginator;
 
-	primengTableHelper: PrimengTableHelper;
+  primengTableHelper: PrimengTableHelper;
 
-	rows = [
-		{ parliament: 'Besut', state: 'Terengganu', status: 'Aktif' },
-		{ parliament: 'Dungun', state: 'Terengganu', status: 'Aktif' },
-		{ parliament: 'KEmaman', state: 'Terengganu', status: 'Aktif' }
-	];
+  filterText = '';
+  filter: any;
+  states: any[];
 
-	constructor(config: NgbModalConfig, private modalService: NgbModal) {
-		this.primengTableHelper = new PrimengTableHelper();
-		config.backdrop = 'static';
-		config.keyboard = false;
-	}
+  constructor(
+    config: NgbModalConfig,
+    private modalService: NgbModal,
+    private _refParlimenServiceProxy: RefParlimenServiceProxy,
+    private _refNegeriServiceProxy: RefNegeriServiceProxy
+  ) {
+    this.primengTableHelper = new PrimengTableHelper();
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
-	ngAfterViewInit(): void {
-		//this.primengTableHelper.adjustScroll(this.dataTable);
-	}
+  ngOnInit(): void {
+    this.negeri();
+  }
 
-	getApplication(event?: LazyLoadEvent) {
-		if (this.primengTableHelper.shouldResetPaging(event)) {
-			this.paginator.changePage(0);
-			return;
-		}
+  getParlimen(event?: LazyLoadEvent) {
+    if (this.primengTableHelper.shouldResetPaging(event)) {
+      this.paginator.changePage(0);
+      return;
+    }
 
-		this.primengTableHelper.showLoadingIndicator();
-		this.primengTableHelper.totalRecordsCount = this.rows.length;
-		this.primengTableHelper.records = this.rows;
-		this.primengTableHelper.hideLoadingIndicator();
-	}
+    this.primengTableHelper.showLoadingIndicator();
+    this._refParlimenServiceProxy
+      .getAll(
+        this.filterText,
+        this.primengTableHelper.getSorting(this.dataTable),
+        this.primengTableHelper.getSkipCount(this.paginator, event),
+        this.primengTableHelper.getMaxResultCount(this.paginator, event)
+      )
+      .subscribe((result) => {
+        this.primengTableHelper.totalRecordsCount = result.total_count;
+        this.primengTableHelper.records = result.items;
+        this.primengTableHelper.hideLoadingIndicator();
+      });
+  }
 
-	reloadPage(): void {
-		this.paginator.changePage(this.paginator.getPage());
-	}
+  negeri() {
+    this._refNegeriServiceProxy
+      .getRefNegeriForDropdown(this.filter)
+      .subscribe((result) => {
+        this.states = result.items.map((data) => {
+          return data.nama_negeri;
+        });
+      });
+  }
 
-	addParliamentModal() {
-		this.modalService.open(TambahEditParlimenComponent, { size: 'lg' });
-	}
+  getNegeri(id) {
+    return this.states[id - 1];
+  }
+
+  reloadPage(): void {
+    this.paginator.changePage(this.paginator.getPage());
+  }
+
+  addParliamentModal() {
+    const modalRef = this.modalService.open(TambahEditParlimenComponent, { size: 'lg' });
+    modalRef.componentInstance.name = 'add';
+    modalRef.result.then((response) => {
+      if (response) {
+        this.getParlimen();
+      }
+    });
+  }
+
+  editParliamentModal(id) {
+    const modalRef = this.modalService.open(TambahEditParlimenComponent, { size: 'lg' });
+    modalRef.componentInstance.name = 'edit';
+    modalRef.componentInstance.id = id;
+    modalRef.result.then((response) => {
+      if (response) {
+        this.getParlimen();
+      }
+    });
+  }
+
 }
