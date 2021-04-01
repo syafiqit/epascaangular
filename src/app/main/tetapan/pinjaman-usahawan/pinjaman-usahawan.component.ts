3436901355dc2620e383b5dbcacbda89c0390799
syafiqit <syafiq.asyraf.it@gmail.com>
2021-04-01 09:25:14 +0800

    feat: consume api for tetapan pinjaman usahawan

--- src/app/main/tetapan/pinjaman-usahawan/pinjaman-usahawan.component.ts
+++ src/app/main/tetapan/pinjaman-usahawan/pinjaman-usahawan.component.ts
@@ -5,6 +5,7 @@ import { LazyLoadEvent } from 'primeng/api';
 import { Paginator } from 'primeng/paginator';
 import { Table } from 'primeng/table';
 import { PrimengTableHelper } from 'src/app/shared/helpers/PrimengTableHelper';
+import {RefPinjamanPerniagaanServiceProxy} from "../../../shared/proxy/service-proxies";
 
 @Component({
 	selector: 'app-pinjaman-usahawan',
@@ -18,14 +19,13 @@ export class PinjamanUsahawanComponent implements AfterViewInit {
 
 	primengTableHelper: PrimengTableHelper;
 
-	rows = [
-		{ name: 'Pembuatan', status: 'Aktif' },
-		{ name: 'Perkhidmatan', status: 'Aktif ' },
-		{ name: 'Pertanian dan Perusahaan Asas Tani', status: 'Aktif' },
-		{ name: 'Peruncitan', status: 'Aktif' }
-	];
+  filterText = '';
 
-	constructor(config: NgbModalConfig, private modalService: NgbModal) {
+	constructor(
+	  config: NgbModalConfig,
+    private modalService: NgbModal,
+    private _refPinjamanPerniagaanServiceProxy: RefPinjamanPerniagaanServiceProxy,
+  ) {
 		config.backdrop = 'static';
 		config.keyboard = false;
 		this.primengTableHelper = new PrimengTableHelper();
@@ -34,27 +34,47 @@ export class PinjamanUsahawanComponent implements AfterViewInit {
 	addEntrepreneurModal() {
 		const modalRef = this.modalService.open(TambahEditPinjamanUsahawanComponent, { size: 'lg' });
 		modalRef.componentInstance.name = 'add';
+    modalRef.result.then((response) => {
+      if (response) {
+        this.getPinjaman();
+      }
+    });
 	}
 
-	editEntrepreneurModal() {
+	editEntrepreneurModal(id) {
 		const modalRef = this.modalService.open(TambahEditPinjamanUsahawanComponent, { size: 'lg' });
 		modalRef.componentInstance.name = 'edit';
+    modalRef.componentInstance.id = id;
+    modalRef.result.then((response) => {
+      if (response) {
+        this.getPinjaman();
+      }
+    });
 	}
 
 	ngAfterViewInit(): void {
 		//this.primengTableHelper.adjustScroll(this.dataTable);
 	}
 
-	getApplication(event?: LazyLoadEvent) {
-		if (this.primengTableHelper.shouldResetPaging(event)) {
-			this.paginator.changePage(0);
-			return;
-		}
+	getPinjaman(event?: LazyLoadEvent) {
+    if (this.primengTableHelper.shouldResetPaging(event)) {
+      this.paginator.changePage(0);
+      return;
+    }
 
-		this.primengTableHelper.showLoadingIndicator();
-		this.primengTableHelper.totalRecordsCount = this.rows.length;
-		this.primengTableHelper.records = this.rows;
-		this.primengTableHelper.hideLoadingIndicator();
+    this.primengTableHelper.showLoadingIndicator();
+    this._refPinjamanPerniagaanServiceProxy
+      .getAll(
+        this.filterText,
+        this.primengTableHelper.getSorting(this.dataTable),
+        this.primengTableHelper.getSkipCount(this.paginator, event),
+        this.primengTableHelper.getMaxResultCount(this.paginator, event)
+      )
+      .subscribe((result) => {
+        this.primengTableHelper.totalRecordsCount = result.total_count;
+        this.primengTableHelper.records = result.items;
+        this.primengTableHelper.hideLoadingIndicator();
+      });
 	}
 
 	reloadPage(): void {
