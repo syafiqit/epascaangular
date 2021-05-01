17069276cbb14ae24d0137444b610814882c741b
Zaki Zakaria <zakihelmi1997@gmail.com>
2021-05-01 18:33:43 +0800

    feat: consume API for tukar-kata-laluan

--- src/app/akaun/tukar-kata-laluan/tukar-kata-laluan.component.ts
+++ src/app/akaun/tukar-kata-laluan/tukar-kata-laluan.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthServiceProxy, InputResetPasswordDto } from 'src/app/shared/proxy/service-proxies';
+declare let require;
+const Swal = require('sweetalert2');
 
 @Component({
 	selector: 'app-tukar-kata-laluan',
@@ -8,10 +12,39 @@ import { Component, OnInit } from '@angular/core';
 export class TukarKataLaluanComponent implements OnInit {
 	public show = false;
 	public showNew = false;
+	allowChangePassword = false;
+	showMessage = false;
+	userEmail: string;
+	accessCode: string;
+	error = '';
 
-	constructor() {}
+  resetPassword: InputResetPasswordDto = new InputResetPasswordDto();
+
+	constructor(
+    private _authServiceProxy: AuthServiceProxy,
+    private _activatedRoute: ActivatedRoute
+  ) {
+    this._activatedRoute.queryParams.subscribe((p) => {
+			this.userEmail = p['emel'];
+			this.accessCode = p['kod_akses'];
+			this.verifyCode(p['emel'], p['kod_akses']);
+		});
+  }
 
 	ngOnInit() {}
+
+  verifyCode(emel, kod_akses) {
+		this._authServiceProxy.verifyCode(emel, kod_akses).subscribe(
+			(e) => {
+				this.allowChangePassword = true;
+				console.log('kod akses sah');
+			},
+			(err) => {
+				this.showMessage = true;
+				this.error = 'Kod Akses Anda Tidak Sah Dan Telah Tamat Tempoh. Sila Minta Kod Akses Yang Baru';
+			}
+		);
+	}
 
 	showPassword() {
 		this.show = !this.show;
@@ -19,5 +52,33 @@ export class TukarKataLaluanComponent implements OnInit {
 
 	showNewPassword() {
 		this.showNew = !this.showNew;
+	}
+
+  save() {
+		this.resetPassword.emel = this.userEmail;
+		this.resetPassword.kod_akses = this.accessCode;
+
+		if (this.resetPassword.kata_laluan === this.resetPassword.ulang_kata_laluan) {
+			this._authServiceProxy.resetPassword(this.resetPassword).subscribe(
+				() => {
+					Swal.fire(
+						'',
+						'Kata Laluan Telah Ditetapkan Semula. Sila Log Masuk Dengan Kata Laluan Baru Anda',
+						'success'
+					).then(() => {
+						location.href = '/akaun/log-masuk';
+					});
+				},
+				() => {
+					Swal.fire(
+						'',
+						'Kod Akses Tidak Sah & Telah Tamat Tampoh. Sila Minta Kod Akses Yang Baharu ',
+						'error'
+					);
+				}
+			);
+		} else {
+			Swal.fire('', 'Kata Laluan Dan Ulang Kata Laluan Tidak Sama ', 'error');
+		}
 	}
 }
