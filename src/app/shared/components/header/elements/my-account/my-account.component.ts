ededc4e9a843b12571adde8aa77c9dedf451dba5
Acipzzz <hasif.yaacob98@gmail.com>
2021-05-03 08:21:58 +0800

    fix: consume api for profile header

--- src/app/shared/components/header/elements/my-account/my-account.component.ts
+++ src/app/shared/components/header/elements/my-account/my-account.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { GetProfilDto, PenggunaProfilDto, SessionServiceProxy } from 'src/app/shared/proxy/service-proxies';
 import { AppAuthService } from 'src/app/shared/services/app-auth-service';
-import { AppSessionService } from 'src/app/shared/services/app-session.service';
 import { TukarKataLaluanComponent } from '../../../../../main/maklumat-akaun/tukar-kata-laluan/tukar-kata-laluan.component';
 
 @Component({
@@ -24,7 +23,6 @@ export class MyAccountComponent implements OnInit {
 		config: NgbModalConfig,
 		private modalService: NgbModal,
 		private _appAuthService: AppAuthService,
-		private _appSessionService: AppSessionService,
     private _sessionServiceProxy: SessionServiceProxy
 	) {
 		// customize default values of modals used by this component tree
@@ -35,19 +33,15 @@ export class MyAccountComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		this.getSessionDetails();
     this.show();
 	}
 
 	show(): void {
 		this._sessionServiceProxy.getProfil().subscribe((result) => {
 			this.getProfil = result;
+      this.name = result.pengguna.nama;
+      this.emel = result.pengguna.emel;
 		});
-	}
-
-	getSessionDetails() {
-		this.name = this._appSessionService.role;
-		this.emel = this._appSessionService.role;
 	}
 
 	openChangePasswordModal() {
