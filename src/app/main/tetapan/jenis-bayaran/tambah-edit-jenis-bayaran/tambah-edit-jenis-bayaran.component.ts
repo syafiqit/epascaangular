5478d3905baf92cbe6994b9039ef90a448f8772e
syafiqit <syafiq.asyraf.it@gmail.com>
2021-09-24 10:57:41 +0800

    feat: consume api for tetapan jenis bayaran

--- src/app/main/tetapan/jenis-bayaran/tambah-edit-jenis-bayaran/tambah-edit-jenis-bayaran.component.ts
+++ src/app/main/tetapan/jenis-bayaran/tambah-edit-jenis-bayaran/tambah-edit-jenis-bayaran.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { NgbActiveModal, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { finalize } from 'rxjs/operators';
+import { swalSuccess } from '@shared/sweet-alert/swal-constant';
+import { CreateOrEditRefJenisBayaranDto, RefJenisBayaranServiceProxy } from '@app/shared/proxy/service-proxies';
 
 @Component({
   selector: 'app-tambah-edit-jenis-bayaran',
@@ -9,11 +12,47 @@ export class TambahEditJenisBayaranComponent implements OnInit {
   @Input() name;
 	@Input() id;
 
-  constructor(
-    public activeModal: NgbActiveModal
-  ) { }
+	jenis_bayaran: CreateOrEditRefJenisBayaranDto = new CreateOrEditRefJenisBayaranDto();
+	saving = true;
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private modalService: NgbModal,
+		public activeModal: NgbActiveModal,
+		private _refJenisBayaranServiceProxy: RefJenisBayaranServiceProxy
+	) {}
+
+	ngOnInit(): void {
+		this.show();
+	}
+
+  show() {
+		if (!this.id) {
+			this.jenis_bayaran = new CreateOrEditRefJenisBayaranDto();
+		} else {
+			this._refJenisBayaranServiceProxy.getRefJenisBayaranForEdit(this.id).subscribe((result) => {
+				this.jenis_bayaran = result.ref_jenis_bayaran;
+			});
+		}
+	}
+
+	save(): void {
+		this.saving = true;
+
+		this._refJenisBayaranServiceProxy
+			.createOrEdit(this.jenis_bayaran)
+			.pipe(
+				finalize(() => {
+					this.saving = false;
+				})
+			)
+			.subscribe(() => {
+				if (this.name == 'add') {
+					swalSuccess.fire('Berjaya!', 'Maklumat Jenis Bayaran Berjaya Ditambah.', 'success');
+				} else if (this.name == 'edit') {
+					swalSuccess.fire('Berjaya!', 'Maklumat Jenis Bayaran Berjaya Dikemaskini.', 'success');
+				}
+				this.activeModal.close(true);
+			});
+	}
 
 }
