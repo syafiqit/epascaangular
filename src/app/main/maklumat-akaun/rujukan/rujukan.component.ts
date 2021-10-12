12ad35d09c00802f243e687c524472dec280743a
Acipzzz <hasif.yaacob98@gmail.com>
2021-10-12 23:22:54 +0800

    feature: consume api for view all active rujukan

--- src/app/main/maklumat-akaun/rujukan/rujukan.component.ts
+++ src/app/main/maklumat-akaun/rujukan/rujukan.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { PrimengTableHelper } from '@app/shared/helpers/PrimengTableHelper';
+import { RefRujukanServiceProxy } from '@app/shared/proxy/service-proxies';
+import { LazyLoadEvent } from 'primeng/api';
+import { Paginator } from 'primeng/paginator';
+import { Table } from 'primeng/table';
+import { finalize } from 'rxjs/operators';
 @Component({
 	selector: 'app-rujukan',
-	templateUrl: './rujukan.component.html'
+	templateUrl: './rujukan.component.html',
+	encapsulation: ViewEncapsulation.None,
+	providers: [RefRujukanServiceProxy]
 })
 export class RujukanComponent implements OnInit {
-	constructor() {}
+	@ViewChild('dataTable', { static: true }) dataTable: Table;
+	@ViewChild('paginator', { static: true }) paginator: Paginator;
 
-	ngOnInit(): void {}
+	primengTableHelper: PrimengTableHelper;
+  defaultImageUrl = '/assets/images/other-images/pdf-symbol.png';
+  filter: string;
+  filterStatus: number = 1;
+  sorting: string;
+  skipCount: number;
+  maxResultCount: number;
 
-	test() {
-		window.open('assets/images/ManualPengguna.pdf');
+	constructor(
+    private _refRujukanServiceProxy: RefRujukanServiceProxy
+  ) {
+		this.primengTableHelper = new PrimengTableHelper();
+  }
+
+	ngOnInit(): void {
+    this.getRujukan();
+  }
+
+	getRujukan(event?: LazyLoadEvent) {
+    this.primengTableHelper.showLoadingIndicator();
+    this._refRujukanServiceProxy
+      .getAll(this.filter, this.filterStatus, this.sorting, this.skipCount, this.maxResultCount)
+      .pipe(finalize(()=>{
+        this.primengTableHelper.hideLoadingIndicator();
+      }))
+      .subscribe((result) => {
+        this.primengTableHelper.totalRecordsCount = result.total_count;
+        this.primengTableHelper.records = result.items;
+      });
 	}
+
+  getFile(location){
+    window.open(location);
+  }
 }
