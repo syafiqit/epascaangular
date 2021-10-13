b063c77bed2bc3d3ad7cdf5cf0773000a747e39c
Acipzzz <hasif.yaacob98@gmail.com>
2021-10-13 15:14:21 +0800

    feature: consume api delete for user management

--- src/app/shared/proxy/service-proxies.ts
+++ src/app/shared/proxy/service-proxies.ts
@@ -22991,6 +22991,67 @@ export class UserServiceProxy {
         }
         return _observableOf<OutputChangeEmelPasswordDto>(<any>null);
     }
+
+    /**
+     * delete User-User
+     * @param id User Id
+     * @return Success
+     */
+    delete(id: number): Observable<OutputChangeEmelPasswordDto> {
+        let url_ = this.baseUrl + "/api/user/delete?";
+        if (id === undefined || id === null)
+            throw new Error("The parameter 'id' must be defined and cannot be null.");
+        else
+            url_ += "id=" + encodeURIComponent("" + id) + "&";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.request("delete", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processDelete(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processDelete(<any>response_);
+                } catch (e) {
+                    return <Observable<OutputChangeEmelPasswordDto>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<OutputChangeEmelPasswordDto>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processDelete(response: HttpResponseBase): Observable<OutputChangeEmelPasswordDto> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = OutputChangeEmelPasswordDto.fromJS(resultData200);
+            return _observableOf(result200);
+            }));
+        } else if (status === 500) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("Internal error has occured", status, _responseText, _headers);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<OutputChangeEmelPasswordDto>(<any>null);
+    }
 }
 
 export class AuthChangePasswordDto implements IAuthChangePasswordDto {
